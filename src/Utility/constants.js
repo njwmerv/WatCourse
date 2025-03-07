195c94a741303b96dda3ac72ba7ec9f98e0c93fe
Mari Montoya <kulafumon@gmail.com>
2025-03-07 09:52:08 -0500

    Fixed autorefresh.

--- src/Utility/constants.js
+++ src/Utility/constants.js
@@ -1,3 +1,10 @@
 // UW API
 export const UW_API_KEY = import.meta.env.VITE_UW_API_KEY;
 export const UW_API_URL = 'https://openapi.data.uwaterloo.ca/v3/';
+
+// Terms
+export const TERM_OPTIONS = [
+	{value:1251, label:'Winter 2025'},
+	{value:1255, label:'Spring 2025'},
+	{value:1259, label:'Fall 2025'}
+];
